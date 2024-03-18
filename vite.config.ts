114a95fdcd142e35b597cb96bcaacb18358420e1
Predeep <predeepkumar.us2022cse@sece.ac.in>
2024-03-18 20:34:31 +0530

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   server:{
-    port:5173,
-    strictPort: true,
-    open: true,
-    proxy: {
-      '/api':{
-        target:'https://pwdapi.rajvikash-r2022cse.workers.dev',
-        changeOrigin: true,
-        rewrite:(path) => path.replace(/^\/api/,''),
-      },
-    },
+  //  server:{
+  //   port:5173,
+  //   strictPort: true,
+  //   open: true,
+  //   proxy: {
+  //     '/api':{
+  //       target:'https://pwdapi.rajvikash-r2022cse.workers.dev',
+  //       changeOrigin: true,
+  //       rewrite:(path) => path.replace(/^\/api/,''),
+  //     },
+  //   },
 
 
-   },
+  //  },
   plugins: [react()],
 })
